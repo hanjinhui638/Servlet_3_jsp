aba1ba4090db335b7b6136be2139ff37e19c7af4
hanjinhui638 <jh312jh@naver.com>
2019-10-31 15:35:34 +0900

    aa

--- src/com/jh/member/MemberDAO.java
+++ src/com/jh/member/MemberDAO.java
@@ -1,5 +1,28 @@
 package com.jh.member;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+
 public class MemberDAO {
+	
+	public int memberJoin(Connection con, MemberDTO memberDTO) throws Exception{
+		int result = 0;
+		
+		String sql = "insert into member values(?,?,?,?,?,1)";
+		PreparedStatement st = con.prepareStatement(sql);
+		st.setString(1, memberDTO.getId());
+		st.setString(2, memberDTO.getPw());
+		st.setString(3, memberDTO.getName());
+		st.setString(4, memberDTO.getEmail());
+		st.setString(5, memberDTO.getPhone());
+	
+		result = st.executeUpdate();
+		
+		st.close();
+		
+		return result;
+		
+		
+	}
 
 }
